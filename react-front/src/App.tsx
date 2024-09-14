2077c4e93647638303614c9c3a6333d6166a584b
Valenthy24rg <valenthy24rg@gmail.com>
2024-09-14 17:29:54 -0500

    change

--- react-front/src/App.tsx
+++ react-front/src/App.tsx
@@ -7,7 +7,7 @@ import EditForm from "./Components/EditForm/EditForm";
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="https://valenthy24rg.github.io/contact-us-fullstack/">
         <Routes>
           <Route path="/" element={ <ShowForm/> } />
           <Route path="/create" element={ <CreateForm/> } />
